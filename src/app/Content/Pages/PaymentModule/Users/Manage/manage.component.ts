e50fe5449c11f45b428f4bd9681e9dd325035bb6
Sajjad Hussain <32987908+SajjadHussainLuqman@users.noreply.github.com>
2019-03-29 23:25:47 +0400

    Loop through all form controls Validations

--- src/app/Content/Pages/PaymentModule/Users/Manage/manage.component.ts
+++ src/app/Content/Pages/PaymentModule/Users/Manage/manage.component.ts
@@ -18,11 +18,11 @@ export class ManageComponent implements OnInit {
 
     this.pageModel = this._formBuilder.group({
       myFullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(8)]],
-      myEmail: ['',[Validators.required,Validators.email]],
+      myEmail: ['', [Validators.required, Validators.email]],
       mySkills: this._formBuilder.group({
-        skillName: [''],
-        experienceInYears: [''],
-        proficiency: ['']
+        skillName: ['', [Validators.required]],
+        experienceInYears: ['', [Validators.required]],
+        proficiency: ['', [Validators.required]]
       }),
     });
 
@@ -30,6 +30,7 @@ export class ManageComponent implements OnInit {
 
   onSubmit() {
     this.logKeyValuePairs(this.pageModel);
+    console.log(this.formErrors);
   }
 
   logKeyValuePairs(group: FormGroup): void {
@@ -49,11 +50,58 @@ export class ManageComponent implements OnInit {
       }
       else {
         console.log('Key = ' + key + ' && Value = ' + abstractControl.value);
+
+        // Clear the existing validation errors
+        this.formErrors[key] = '';
+        if (abstractControl && !abstractControl.valid) {
+          // Get all the validation messages of the form control
+          // that has failed the validation
+          const messages = this.validationMessages[key];
+          // Find which validation has failed. For example required,
+          // minlength or maxlength. Store that error message in the
+          // formErrors object. The UI will bind to this object to
+          // display the validation errors
+          for (const errorKey in abstractControl.errors) {
+            if (errorKey) {
+              this.formErrors[key] += messages[errorKey] + ' ';
+            }
+          }
+        }
       }
 
     });
   }
 
+  // Notice, each key in this object has the same name as the
+  // corresponding form control
+  formErrors = {
+    'myFullName': '',
+    'myEmail': '',
+    'skillName': '',
+    'experienceInYears': '',
+    'proficiency': ''
+  };
+
+  // This object contains all the validation messages for this form
+  validationMessages = {
+    'myFullName': {
+      'required': 'Full Name is required.',
+      'minlength': 'Full Name must be greater than 2 characters.',
+      'maxlength': 'Full Name must be less than 8 characters.'
+    },
+    'myEmail': {
+      'required': 'Email is required.'
+    },
+    'skillName': {
+      'required': 'Skill Name is required.',
+    },
+    'experienceInYears': {
+      'required': 'Experience is required.',
+    },
+    'proficiency': {
+      'required': 'Proficiency is required.',
+    },
+  };
 
   onLoadDataClick() {
     this.pageModel.setValue({
@@ -65,13 +113,5 @@ export class ManageComponent implements OnInit {
         proficiency: 'beginner'
       }
     });
-
-    // Partial Load then Use patchValue
-
-    // this.pageModel.patchValue({
-    //   myFullName: 'Sajjad Hussain',
-    //   myEmail: 'sajjadlogic@live.com'
-    // });
-
   }
 }
